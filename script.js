8d67d8a812259f824aadcdb2f62735d84e8e0540
metroliv <135633599+metroliv@users.noreply.github.com>
2024-04-01 18:00:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,24 +5,27 @@ function showmenu(){
 function showhome() {
   document.getElementById("home").style.display = "block";
 }
+ function showRecruit() {
+document.getElementById("recruit").style.display = "none";
+}
 
 function showBalance() {
   document.getElementById("home").style.display = "none";
   document.getElementById("recruit").style.display = "none";
-  document.getElementById("balance").style.display = "block";
+  
   document.getElementById("team").style.display = "none";
 }
 
 function showTeam() {
   document.getElementById("home").style.display = "none";
   document.getElementById("recruit").style.display = "none";
-  document.getElementById("balance").style.display = "none";
+  
   document.getElementById("team").style.display = "block";
 }
 
 document.getElementById("recruitLink").addEventListener("click", showRecruit);
 document.getElementById("homeBtn").addEventListener("click", showHome);
-document.getElementById("balanceBtn").addEventListener("click", showBalance);
+
 document.getElementById("teamBtn").addEventListener("click", showTeam);
 
 function copyLink() {
